cfa54d8f16abf290f03cfd924644703de3a33217
NadavAharoni <nadav.aharoni@gmail.com>
2025-09-15 22:32:43 +0300

    list examples

---
+++ examples/list_examples.py
@@ -0,0 +1,32 @@
+list1 = [ None ] * 6
+# print( list1 )
+
+empty_list = []
+list1 = [ empty_list ] * 5
+print( F"list1 = [ empty_list ] * 5 = ", list1 )
+print()
+
+for i, sub_list in enumerate(list1):
+    sub_list.append(i)
+    list1[i] = sub_list.copy()
+
+for l in list1:
+    print(l)
+
+# print("=====")
+# list1 = [ [] for i in range(5) ]
+# print( list1 )
+# for i, sub_list in enumerate(list1):
+#     sub_list += [i]
+
+# for l in list1:
+#     print(l)
+
+# print("=====")
+# list1 = [ [] for i in range(5) ]
+# for i, sub_list in enumerate(list1):
+#     new_list = list1[i] + [i]
+#     list1[i] = new_list
+
+# for l in list1:
+#     print(l)
